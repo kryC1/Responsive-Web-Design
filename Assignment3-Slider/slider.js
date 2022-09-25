67fd306c4ddf998b89d17500f633f6f3fba06cb2
Koray ÇELİK <97339180+kryC1@users.noreply.github.com>
2022-09-25 12:20:07 +0300

    Add files via upload

---
+++ Assignment3-Slider/slider.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+	$("#slider").bxSlider({
+		auto: true,
+		minSlides: 1,
+		maxSlides: 1,
+		slideWidth: 400,
+		slideMargin: 10,
+		randomStart: true,
+		speed: 300,
+		moveSlides: 1,
+		pager: true,
+		pagerType: "short"
+	});
+
+
+
+});
+
